94d7bc1c4c4c93b6b6ab70c118f4f30b7af503f8
Hritik Pathare <97274693+Thehritikpathare@users.noreply.github.com>
2022-03-08 17:28:17 +0530

    Take no.through user and show there no.

---
+++ Tppers no. show.c
@@ -0,0 +1,18 @@
+//toppers list give no. and show there no.
+void main(){
+	int r1, r2, r3, r4, r5;
+	
+	printf("Enter the roll number 1st topper\n");
+	scanf("%d",&r1);
+	printf("Enter the roll number 2nd topper\n");
+	scanf("%d",&r2);
+	printf("Enter the roll number 3rd topper\n");
+	scanf("%d",&r3);
+	printf("Enter the roll number 4th topper\n");
+	scanf("%d",&r4);
+	printf("Enter the roll number 5th topper\n");
+	scanf("%d",&r5);
+	
+	printf("\n\nThe toppers are (roll number) %d %d %d %d %d", r1, r2, r3, r4, r5);
+
+}
